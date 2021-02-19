434253fad45487633e9b62f0113b35aab0c97d49
Akshat Adani <akshatadani99@gmail.com>
2021-02-19 14:48:52 +0530

    share code on github

---
+++ frontend/src/data.js
@@ -0,0 +1,72 @@
+const data ={
+    products: [
+        {
+            id:1,
+            name:'Nike Slim Shirt',
+            category:'Shirts',
+            image:'/images/p1.jpg',
+            price:899,
+            brand:'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            id:2,
+            name:'Peter-England Shirt',
+            category:'Shirts',
+            image:'/images/p2.jpg',
+            price:899,
+            brand:'Peter-England',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            id:3,
+            name:'UCB Shirt',
+            category:'Shirts',
+            image:'/images/p3.jpg',
+            price:899,
+            brand:'UCB',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            id:4,
+            name:'Peter-England pant',
+            category:'Pants',
+            image:'/images/p4.jpg',
+            price:1199,
+            brand:'Allen-Solly',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            id:5,
+            name:'Louis-Phillepe pant',
+            category:'Pants',
+            image:'/images/p5.jpg',
+            price:1599,
+            brand:'',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            id:6,
+            name:'U.S.Polo pant',
+            category:'Pants',
+            image:'/images/p6.jpg',
+            price:1299,
+            brand:'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        }
+    ]
+};
+
+export default data;
