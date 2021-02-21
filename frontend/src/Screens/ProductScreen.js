82fa51057218162a8ef955d56c4bb84d1c94ba8b
Akshat Adani <akshatadani99@gmail.com>
2021-02-21 23:30:10 +0530

    ädd to cart event handling

--- frontend/src/Screens/ProductScreen.js
+++ frontend/src/Screens/ProductScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Rating from '../components/rating';
 import {  useDispatch, useSelector } from 'react-redux';
 import {Link} from 'react-router-dom';
@@ -9,12 +9,17 @@ import { detailsProduct } from '../actions/productActions';
 export default function ProductScreen(props){
     const dispatch = useDispatch();
     const productID = props.match.params.id;
+    const [qty, setQty] = useState(1);
     const   productDetails = useSelector((state) => state.productDetails);
     const {loading, error, product} = productDetails;
 
     useEffect(()=>{
         dispatch(detailsProduct(productID));
     }, [dispatch, productID]);
+
+    const addToCartHandler = () => {
+        props.history.push(`/cart/${productID}?qty=${qty}`);
+    }
 
     return(
         <div>
@@ -65,9 +70,27 @@ export default function ProductScreen(props){
                                   </div>
                               </div>
                           </li>
-                          <li>
-                              <button className="primary block">Add to Cart</button>
-                          </li>
+                           {
+                              product.countInStock > 0 && (
+                                  <>
+                                    <div className="row">
+                                        <div>Qty</div>
+                                        <div>
+                                            <select value={qty} onChange={e => setQty(e.target.value)}>
+                                                {[...Array(product.countInStock).keys()].map(
+                                                    (x) => (
+                                                        <option key={x + 1} value={x + 1}>{x +1}</option>
+                                                    )
+                                                )}
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <li>
+                                    <button onClick={addToCartHandler} className="primary block">Add to Cart</button>
+                                    </li>
+                                  </>
+                                )
+                            }
                       </ul>
                   </div>
               </div>
@@ -77,6 +100,7 @@ export default function ProductScreen(props){
         </div>
     );
 }
+
     
 
                 
