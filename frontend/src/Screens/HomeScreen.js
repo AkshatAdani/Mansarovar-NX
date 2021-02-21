9b1e3e2ee942182fdaf09c8d7580a49165cee472
Akshat Adani <akshatadani99@gmail.com>
2021-02-21 11:11:58 +0530

    Eslint

--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import Product from '../components/product';
 import axios from 'axios';
 import MessageBox from '../components/MessageBox';
