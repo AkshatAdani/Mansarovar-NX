2233c1e8f405ee5d6190108b6acdbfa64e1df3dd
AkshatAdani <akshatadani99@gmail.com>
2021-04-22 15:31:35 +0530

    Update product

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,7 +1,7 @@
 import {applyMiddleware, combineReducers, compose, createStore} from 'redux';
 import thunk from 'redux-thunk';
 import { cartReducer } from './reducers/CartReducers';
-import { productCreateReducer, productDetailsReducer, productListReducer } from './reducers/productReducer';
+import { productCreateReducer, productDetailsReducer, productListReducer, productUpdateReducer } from './reducers/productReducer';
 import { userDetailsReducer, userRegisterReducer, userSigninReducer, userUpdateProfileReducer } from './reducers/userReducers';
 import {orderCreateReducer,orderDetailsReducer,  orderMineListReducer, orderPayReducer,} from './reducers/orderReducers';
 
@@ -34,6 +34,7 @@ const reducer = combineReducers({
     userDetails: userDetailsReducer,
     userUpdateProfile: userUpdateProfileReducer,
     productCreate: productCreateReducer,
+    productUpdate: productUpdateReducer,
 });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
