32dbbf77736e50ad003aa80a967fec0f2280581f
AkshatAdani <akshatadani99@gmail.com>
2021-04-22 16:42:30 +0530

    delete order admin

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,7 +3,7 @@ import thunk from 'redux-thunk';
 import { cartReducer } from './reducers/CartReducers';
 import { productCreateReducer, productDeleteReducer, productDetailsReducer, productListReducer, productUpdateReducer } from './reducers/productReducer';
 import { userDetailsReducer, userRegisterReducer, userSigninReducer, userUpdateProfileReducer } from './reducers/userReducers';
-import {orderCreateReducer,orderDetailsReducer,  orderMineListReducer, orderPayReducer,} from './reducers/orderReducers';
+import {orderCreateReducer,orderDeleteReducer,orderDetailsReducer,  orderListReducer,  orderMineListReducer, orderPayReducer,} from './reducers/orderReducers';
 
 const initialState = {
     userSignin: {
@@ -36,6 +36,8 @@ const reducer = combineReducers({
     productCreate: productCreateReducer,
     productUpdate: productUpdateReducer,
     productDelete: productDeleteReducer,
+    orderList: orderListReducer,
+    orderDelete: orderDeleteReducer,
 });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
